d41dda9feedd63dcef8ec76ea0ecceefd97418fc
Orelso Vitam <orelso.vitam@avanio.com>
2023-02-28 15:13:00 +0200

    first

--- index.js
+++ index.js
@@ -1,68 +1,55 @@
 const character = document.getElementById('character');
+const blocks = Array.from(document.getElementsByClassName('block'));
 
 let isJumping = false;
 let jumpSpeed = 0;
-let moveSpeed = 5;
-let moveLeft = false;
-let moveRight = false;
 
 function jump() {
-  if (!isJumping) {
-    isJumping = true;
-    jumpSpeed = 15;
-    character.classList.add('jump');
-    setTimeout(() => {
-      character.classList.remove('jump');
-      isJumping = false;
-    }, 500);
-  }
+  isJumping = true;
+  jumpSpeed = 15;
+  character.classList.add('jump');
+  setTimeout(() => {
+    character.classList.remove('jump');
+  }, 300);
 }
 
 function applyGravity() {
-  const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
-  if (characterTop > 300) {
-    jumpSpeed = 0;
-  } else {
-    jumpSpeed -= 1;
+    const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+    const characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
+    let isOnBlock = false;
+  
+    blocks.forEach(block => {
+      const blockTop = parseInt(window.getComputedStyle(block).getPropertyValue('top'));
+      const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+  
+      // check if character is intersecting with the block from above
+      if (blockLeft <= characterLeft && characterLeft <= blockLeft + 60 && characterTop + 60 >= blockTop && characterTop < blockTop) {
+        character.style.top = `${blockTop - 60}px`;
+        isJumping = false;
+        isOnBlock = true;
+      }
+    });
+  
+    if (characterTop > 300 && !isOnBlock) {
+      jumpSpeed = 0;
+      isJumping = false;
+    } else {
+      jumpSpeed -= 1;
+    }
+    character.style.top = `${characterTop - jumpSpeed}px`;
   }
-  character.style.top = `${characterTop - jumpSpeed}px`;
-}
-
-function applyMovement() {
-  const characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
-  if (moveLeft && characterLeft > 0) {
-    character.style.left = `${characterLeft - moveSpeed}px`;
-  }
-  if (moveRight && characterLeft < 3060) {
-    character.style.left = `${characterLeft + moveSpeed}px`;
-  }
-}
+  
 
 document.addEventListener('keydown', event => {
-  if (event.code === 'ArrowUp') {
+  if (event.code === 'ArrowUp' && !isJumping) {
     jump();
   }
-  if (event.code === 'ArrowLeft') {
-    moveLeft = true;
-  }
   if (event.code === 'ArrowRight') {
-    moveRight = true;
+    character.style.left = `${parseInt(window.getComputedStyle(character).getPropertyValue('left')) + 10}px`;
+  }
+  if (event.code === 'ArrowLeft') {
+    character.style.left = `${parseInt(window.getComputedStyle(character).getPropertyValue('left')) - 10}px`;
   }
 });
 
-document.addEventListener('keyup', event => {
-  if (event.code === 'ArrowLeft') {
-    moveLeft = false;
-  }
-  if (event.code === 'ArrowRight') {
-    moveRight = false;
-  }
-});
-
-function animate() {
-  applyGravity();
-  applyMovement();
-  requestAnimationFrame(animate);
-}
-
-animate();
+setInterval(applyGravity, 10);
