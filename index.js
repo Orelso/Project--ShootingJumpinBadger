064b2142e26d9648437787fa88becd6945252a2a
Orelso Vitam <orelso.vitam@avanio.com>
2023-03-01 12:33:53 +0200

    new

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-// Define the warrior and mage objects
-const warrior = {
-  name: 'Warrior',
+const tunk = {
+  name: "Tunk",
+  class: "Melee",
   health: 100,
   strength: 10,
-  defense: 5,
-  speed: 5
+  defense: 8,
+  speed: 5,
+  ability: "Charge"
 };
 
-const mage = {
-  name: 'Mage',
-  health: 100,
-  strength: 5,
-  defense: 2,
-  speed: 10
+const mesta = {
+  name: "Mesta",
+  class: "Ranged",
+  health: 90,
+  strength: 8,
+  defense: 6,
+  speed: 7,
+  ability: "Critical Strike"
 };
 
-// Define a battle function
-function battle(attacker, defender) {
-  // Calculate damage
+
+const tunkAttackBtn = document.getElementById("tunk-attack");
+const mestaAttackBtn = document.getElementById("mesta-attack");
+
+tunkAttackBtn.addEventListener("click", function() {
+  attack(tunk, mesta);
+});
+
+mestaAttackBtn.addEventListener("click", function() {
+  attack(mesta, tunk);
+});
+
+function attack(attacker, defender) {
+  // Calculate damage based on attacker's strength and defender's defense
   const damage = attacker.strength - defender.defense;
-
-  // Calculate chance to hit
-  const hitChance = attacker.speed / defender.speed;
-
-  // Check if attack hits
-  if (Math.random() <= hitChance) {
-    // Attack hits
+  if (damage > 0) {
     defender.health -= damage;
-    if (defender.health <= 0) {
-      // Defender is defeated
-      console.log(`${defender.name} has been defeated!`);
-    } else {
-      // Defender survives attack
-      console.log(`${defender.name} takes ${damage} damage!`);
-    }
-  } else {
-    // Attack misses
-    console.log(`${attacker.name}'s attack misses!`);
+  }
+  
+  // Log the attack and the defender's new health
+  const message = `${attacker.name} attacks ${defender.name} for ${damage} damage! ${defender.name} has ${defender.health} health left.`;
+  logMessage(message);
+  
+  // Check if the game has ended
+  if (defender.health <= 0) {
+    endGame(attacker, defender);
   }
 }
 
-// Simulate the battle between warrior and mage
-while (warrior.health > 0 && mage.health > 0) {
-  // Warrior attacks first
-  battle(warrior, mage);
-
-  // Mage attacks second
-  if (mage.health > 0) {
-    battle(mage, warrior);
-  }
+function endGame(winner, loser) {
+  const message = `${winner.name} has defeated ${loser.name}!`;
+  logMessage(message);
+  
+  // Disable attack buttons
+  tunkAttackBtn.disabled = true;
+  mestaAttackBtn.disabled = true;
 }
 
-// Determine the winner
-if (warrior.health <= 0) {
-  console.log(`${mage.name} wins!`);
-} else {
-  console.log(`${warrior.name} wins!`);
-}
+
